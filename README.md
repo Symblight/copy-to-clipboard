ab6c5fc68ae5ff2d771af7133fbc2fc31725c68e
Aleksey <symblight@gmail.com>
2018-10-20 18:25:58 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# @symblight/copy-to-clip
+
+
+[![npm (scoped)](https://img.shields.io/npm/v/:scope/:package.svg)](https://github.com/symblight/copy-to-clip)
+
---
+++ index.js
@@ -0,0 +1,27 @@
+const copyToClipboard = (text) => {
+    const aux = document.createElement('input');
+
+    aux.setAttribute('value', text);
+
+    document.body.appendChild(aux);
+
+    aux.select();
+
+    document.execCommand('copy');
+
+    document.body.removeChild(aux);
+}
+
+const copyFromDOM = (node) => {
+    const range = document.createRange();
+    range.selectNode(node);
+    const sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+
+    window.getSelection();
+
+    document.execCommand('copy');
+
+    window.getSelection().removeAllRanges();
+}
